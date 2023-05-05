7747b1eef58738539c20db3a7672f2551f7d1e92
gracias2983 <gracias2983@gmail.com>
2023-05-05 16:29:50 +0530

    New updates push

--- app.py
+++ app.py
@@ -1,73 +1,89 @@
-
-from flask import Flask, flash, request, redirect, url_for, render_template
-from flask import json, jsonify
-import urllib.request
+from flask import Flask, request, jsonify
 import os
+import urllib.request
+
 from werkzeug.utils import secure_filename
 
 import cv2 as cv
-import numpy as np
+
 import pytesseract
-import sys
+import sys 
+app = Flask(__name__)
  
-app = Flask(__name__,template_folder='templates')
+app.secret_key = "caircocoders-ednalan"
  
-UPLOAD_FOLDER = 'static/uploads/'
- 
-app.secret_key = "secret key"
+UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
  
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
  
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-     
- 
+
+
 @app.route('/')
 def home():
-    return render_template('index.html')
-    # return '<h2 style=text-align:center; > Hola! EveryOne welcome to my Homepage!!</h2>'
+    # return render_template('index.html')
+    return '<h2 style=text-align:center; > Hello!! welcome to my Homepage!!</h2>'
  
 @app.route('/get-text', methods=['POST'])
-def upload_image():
-    # result ={}
+def upload_file():
+    
+    # check if the post request has the file part
     if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No image selected for uploading')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        print('upload_image filename: ' + filename)
-        # print('upload_image fileurl: ' + fileurl)
+        resp = jsonify({'message' : 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+ 
+    files = request.files.getlist('file')
+     
+    errors = {}
+    success = False
+     
+    for file in files:      
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            success = True
 
-        try:
-            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            print("image_path : ",image_path)
-            image = cv.imread(image_path)
-            text = pytesseract.image_to_string(image)
-            print(type(text))
+            try:
+                image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                print("image_path : ",image_path)
+                image = cv.imread(image_path)
+                text = pytesseract.image_to_string(image)
 
-            output = text.strip()
-            output = output.replace("\n'","")
-            output_text = ''.join(output.splitlines())
-            
-            result ={
-            "image_text" : output_text
-            }
-        except Exception as e:
-            print("Img is not found", e)
-            print(f"error pytesserct not installed ! Install this package to resolve above error 'pip3 install pytesseract'")
-            pass
-        return jsonify(result)
-        # return render_template('index.html', filename=filename)
+
+                output = text.strip()
+                output = output.replace("\n'","")
+                final_text = ''.join(output.splitlines())
+                
+                result ={
+                "text" : final_text
+                }
+
+            except Exception as e:
+                print("Img is not found",e)
+                print(f"error pytesserct not installed ! Install this package to resolve above error 'pip3 install pytesseract'")
+                pass
+
+            # return jsonify(result)
+        else:
+            errors[file.filename] = 'File type is not allowed'
+ 
+    if success and errors:
+        errors['message'] = 'File(s) successfully uploaded'
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+    if success:
+        resp = jsonify(result)
+        resp.status_code = 201
+        return resp
     else:
-        flash('Allowed image types are - png, jpg, jpeg, gif')
-        return redirect(request.url)
-
-if __name__ == "__main__":
-    app.run(debug = True )
+        resp = jsonify(errors)
+        resp.status_code = 500
+        return resp
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
